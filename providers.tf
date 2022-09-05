ed65aa3c5952eef6d02cbcc63b84b5ee6244bd69
Anton Khanabeev <anton@vapaus.io>
2022-09-05 09:10:05 +0300

    fix3

--- providers.tf
+++ providers.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-terraform {
-  backend "gcs" {
-    bucket = "vapaus-terraform-staging-bucket"
-    prefix = "terraform/state"
-  }
-}
